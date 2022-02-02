ec119cde84b1c67bdc4f82a26079656f00e48330
Tanveer Ahmad <tahashmi@users.noreply.github.com>
2022-02-02 12:57:11 +0100

    Update download_singularity_images.sh

--- long-reads/download_singularity_images.sh
+++ long-reads/download_singularity_images.sh
@@ -1,1 +1,13 @@
 
+cd /scratch-shared/tahmad/images
+
+#Long reads aligners
+singularity pull docker://quay.io/biocontainers/lra:1.3.2--h00214ad_1
+
+#Long reads variant callers
+singularity pull docker://kishwars/pepper_deepvariant:r0.7
+singularity pull docker://google/deepvariant:1.3.0-gpu
+singularity pull docker://google/deepvariant:1.3.0
+singularity pull docker://ontresearch/medaka:latest
+
+singularity pull docker://dancooke/octopus
